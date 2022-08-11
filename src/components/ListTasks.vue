ff4d92f68addfcbd063255d59dfbb8941d8b17f6
Paresh Parmar <pareshparmar232@gmail.com>
2022-08-11 10:48:48 +0530

    List items

--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -1,9 +1,50 @@
 <template>
-<v-btn>Test List Task</v-btn>
+	<v-card class="mx-auto">
+		<v-card-title>
+			<v-row>
+				<v-col md="12" class="text-center">Tasks List</v-col>
+				<v-col md="6" class="text-center" justify-center>
+					<v-table>
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Name</th>
+								<th>Description</th>
+								<th>Status</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="task in tasks" :key="task.id">
+								<td>{{ task.id }}</td>
+								<td>{{ task.name }}</td>
+								<td>{{ task.description }}</td>
+								<td>{{ task.status }}</td>
+								<td> <v-btn depressed color="primary">Primary</v-btn></td>
+							</tr>
+						</tbody>
+					</v-table>
+				</v-col>
+			</v-row>
+		</v-card-title>
+	</v-card>
 </template>
 <script>
+	import axios from "axios";
+
 	export default {
 		name: "ListTasks",
-		data: () => ({}),
+		data: () => ({
+			tasks: [],
+		}),
+		mounted() {
+			this.getTasks();
+		},
+		methods: {
+			async getTasks() {
+				const data = await axios.get("http://localhost:3001/tasks");
+				this.tasks = data.data;
+			},
+		},
 	};
 </script>
