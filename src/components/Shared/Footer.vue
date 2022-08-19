8dde5a2e33719de6d2eca9ae7ddda8c182815fda
Paresh Parmar <pareshparmar232@gmail.com>
2022-08-19 09:18:43 +0530

    Footer added

--- src/components/Shared/Footer.vue
+++ src/components/Shared/Footer.vue
@@ -9,26 +9,19 @@
       class="indigo lighten-1 white--text text-center"
     >
       <v-card-text>
-        <v-btn
-          v-for="icon in icons"
-          :key="icon"
-          class="mx-4 white--text"
-          icon
-        >
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
           <v-icon size="24px">
             {{ icon }}
           </v-icon>
         </v-btn>
       </v-card-text>
 
-      <v-card-text class="white--text pt-0">
-        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-      </v-card-text>
+      <v-card-text class="white--text pt-0">The purpose of the respository is only to create the CRUD operation for the VueJS framework.</v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        {{ new Date().getFullYear() }} — <strong @click="redirecctToGithub">Made with love by Paresh Parmar</strong>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -36,5 +29,10 @@
 <script>
 export default {
     name : 'Footer',
+    methods: {
+      redirecctToGithub() {
+        window.location = 'https://github.com/P-d0309'
+      }
+    }
 }
 </script>
